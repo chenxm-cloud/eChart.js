15bf3b2713ad9996e58f8b5f06fcc1b47fc6aca0
心叶 <yelloxing@gmail.com>
2019-01-24 19:10:57 +0800

    svg.tree.TB.move

---
+++ data/menu.json.js
@@ -0,0 +1,54 @@
+window.menu = {
+    "name": "前端",
+    "list": [{
+        "name": "核心",
+        "list": [{
+            "name": "HTML"
+        }, {
+            "name": "CSS"
+        }, {
+            "name": "ES"
+        }]
+    }, {
+        "name": "安全"
+    }, {
+        "name": "计算机",
+        "list": [{
+            "name": "算法",
+            "list": [{
+                "name": "思想",
+                "list": [{
+                    "name": "分治"
+                }, {
+                    "name": "回溯"
+                }, {
+                    "name": "备忘录"
+                }, {
+                    "name": "限界"
+                }, {
+                    "name": "贪心"
+                }]
+            }, {
+                "name": "模式"
+            }]
+        }]
+    }, {
+        "name": "工具"
+    }, {
+        "name": "规范"
+    }, {
+        "name": "库",
+        "list": [{
+            "name": "图表",
+            "list": [{
+                "name": "clayJs"
+            }, {
+                "name": "d3Js"
+            }, {
+                "name": "threeJs"
+            }]
+        }, {
+            "name": "jquery"
+        }]
+    }]
+};
